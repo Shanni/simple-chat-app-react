4c86fbd4354ec3828cc5ff9aebcfdea25d3adf69
Shanni <shan.liu03@gmail.com>
2017-10-25 14:27:18 -0700

    init react chat app

---
+++ src/ChatMessage.js
@@ -0,0 +1,7 @@
+var createReactClass = require('create-react-class');
+var React = require('react');
+module.exports = createReactClass({
+  render: function() {
+    return <p>{this.props.message}</p>;
+  }
+});
